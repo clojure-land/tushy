d8870159979866c12a0303bbfff145ccf45d167f
Baishampayan Ghose <b.ghose@infinitelybeta.com>
2010-10-01 16:03:05 +0530

    Added project.clj & .gitignore

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject tushy "0.1-SNAPSHOT"
+  :description "A simple blog aggregrator"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [prolefeed "0.1-SNAPSHOT"]
+                 [enlive "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
