94d4652bca7b370a5d4c3ae11360777d0b2c69d8
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-03 14:06:12 +0530

    Extract feeds and author names from XML file

--- src/tushy/core.clj
+++ src/tushy/core.clj
@@ -1,2 +1,16 @@
-(ns tushy.core)
+(ns tushy.core
+  (:require [clojure.zip :as zip]
+            [clojure.xml :as xml]
+            [clojure.contrib.duck-streams :as streams]
+            [net.cgrand.enlive-html :as html])
+  (:use clojure.contrib.zip-filter.xml))
+
+(defn zip-str [s]
+  (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+
+(def xml-planet-info (zip-str (streams/slurp* "./config.xml.sample")))
+
+(def feed-map (map #(hash-map :author %1 :feed %2)
+                   (xml-> xml-planet-info :blog :author text)
+                   (xml-> xml-planet-info :blog :feed text)))
 
