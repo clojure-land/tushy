dee24ee7f0d45a8e466813555a6e84f7c74a59a7
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-05 11:18:37 +0530

    Removed prolefeed as I'm not using it

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A simple blog aggregrator"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [prolefeed "0.1-SNAPSHOT"]
-                 [enlive "1.0.0-SNAPSHOT"]]
+                 [enlive "1.0.0-SNAPSHOT"]
+                 [clj-time "0.1.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-run "1.0.1-SNAPSHOT"]])
