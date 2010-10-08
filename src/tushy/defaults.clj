37b5754329f10b1b6482ed7e928bf383eded4a1d
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-08 20:11:09 +0530

    Moved site-related information into the config xml file

--- src/tushy/defaults.clj
+++ src/tushy/defaults.clj
@@ -1,8 +1,2 @@
 (ns tushy.defaults)
 
-(def *default-title* "Planet Clojure")
-
-(def *default-site-description* "Copyright &copy; 2009, Planet Clojure. No rights reserved.<br />
-Planet Clojure is maintained by <a href=\"http://freegeek.in/\">Baishamapayan Ghose</a>.<br />
-Clojure and the Clojure logo are Copyright &copy; 2008-2009, Rich Hickey.<br />
-Theme by <a href=\"http://brajeshwar.com/\">Brajeshwar</a>.")
