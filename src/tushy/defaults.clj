171f04a15b74752bbb22e2c41da30aa7daf1abd9
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-11 19:13:26 +0530

    a def for defining the location of the XML configuration file

--- src/tushy/defaults.clj
+++ src/tushy/defaults.clj
@@ -3,6 +3,8 @@
             [clojure.xml :as xml]
             [clojure.contrib.duck-streams :as streams]))
 
+(def *xml-file-name* "/home/vedang/Work/tushy/config.xml.sample")
+
 (defn zip-str
   "Parse an xml file"
   [f]
