df62d748ce2904029b0cd68d0b65793df62ddfb3
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-13 13:57:26 +0530

    Added a selector and a snippet to select and build a section with links

--- src/tushy/template.clj
+++ src/tushy/template.clj
@@ -3,25 +3,35 @@
   (:use [tushy.defaults]
         [clojure.contrib.zip-filter.xml]))
 
-(defn generate-ctxt
+(defn generate-site-info
   "parse the XML configuration file and generate a map with the appropriate values"
   []
   (let [xml-planet-info (zip-str *xml-file-name*)]
-    (map #(zipmap [:site :site_copyright]
-                  [%1 %2])
+    (map #(zipmap [:site_name :site_title :site_copyright]
+                  [%1 %2 %3])
          (xml-> xml-planet-info :site :name text)
+         (xml-> xml-planet-info :site :title text)
          (xml-> xml-planet-info :site :site_copyright text))))
 
-(def *link-sel* [[:#subscriptions (html/nth-of-type 1)] :> html/first-child])
+(def *subscription-link-sel* [[:.links (html/nth-of-type 1)] :> html/first-child])
 
-(html/defsnippet link-model "tushy/html_templates/index.html" *link-sel*
+(html/defsnippet link-model "tushy/html_templates/index.html" *subscription-link-sel*
   [{href :href text :text}]
   [:a] (html/do->
         (html/content text)
         (html/set-attr :href href)))
 
+(def *section-sel* {[:.title] [[:.links (html/nth-of-type 1)]]})
+
+(html/defsnippet section-model "tushy/html_templates/index.html" *section-sel*
+  [{title :title data :data} model]
+  [:.title] (html/content title)
+  [:.links] (html/content (map model data)))
+
 (html/deftemplate index "tushy/html_templates/index.html"
-  [ctxt]
-  [:title] (html/content (get ctxt :site "No site name"))
-  [:h1#title] (html/content (get ctxt :site "No title given"))
+  [ctxt section-data]
+  [:title] (html/content (get ctxt :site_name "No site name"))
+  [:#header :#title] (html/content (get ctxt :site_title "No title given"))
+  [:.sidebar-list#Subscriptions] (html/content (section-model section-data link-model))
   [:footer#footer :p] (html/content (get ctxt :site_copyright "No site description")))
+
