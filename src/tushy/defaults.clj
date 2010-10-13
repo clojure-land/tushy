f4678b2d8a0fb2e2714c72ac0ceb310525666288
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-13 19:35:38 +0530

    extracted global configuration values from config file

--- src/tushy/defaults.clj
+++ src/tushy/defaults.clj
@@ -1,14 +1,18 @@
 (ns tushy.defaults
   (:require [clojure.zip :as zip]
             [clojure.xml :as xml]
-            [clojure.contrib.duck-streams :as streams]))
+            [clojure.contrib.duck-streams :as streams])
+  (:use [clojure.contrib.zip-filter.xml]))
 
 (def *xml-file-name* "/home/vedang/Work/tushy/config.xml.sample")
-
-(def *final-file-name* "/home/vedang/Work/tushy/src/tushy/html_templates/final.html")
 
 (defn zip-str
   "Parse an xml file"
   [f]
   (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes (streams/slurp* f))))))
 
+(def *planet-config-info* (zip-str *xml-file-name*))
+
+(def *template-file-name* (first (xml-> *planet-config-info* :site :html_template text)))
+(def *final-file-name* (first (xml-> *planet-config-info* :site :output_file text)))
+(def *no-of-items* (first (xml-> *planet-config-info* :site :items_per_page text)))
