4e6507de7d21114fdfae1a18069b2217c5491456
Vedang Manerikar <ved.manerikar@gmail.com>
2010-10-13 12:08:54 +0530

    Added a link selector and a snippet that can use this selector

--- src/tushy/template.clj
+++ src/tushy/template.clj
@@ -12,6 +12,14 @@
          (xml-> xml-planet-info :site :name text)
          (xml-> xml-planet-info :site :site_copyright text))))
 
+(def *link-sel* [[:#subscriptions (html/nth-of-type 1)] :> html/first-child])
+
+(html/defsnippet link-model "tushy/html_templates/index.html" *link-sel*
+  [{href :href text :text}]
+  [:a] (html/do->
+        (html/content text)
+        (html/set-attr :href href)))
+
 (html/deftemplate index "tushy/html_templates/index.html"
   [ctxt]
   [:title] (html/content (get ctxt :site "No site name"))
